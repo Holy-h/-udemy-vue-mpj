463384a1b2ce4d456e31554362297861d17622d4
Jake <kepy1106@gmail.com>
2020-11-30 19:56:36 +0900

    ReceivedRequests

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,5 +1,9 @@
 export default {
-  requests(state) {
-    return state.requests;
+  requests(state, _, __, rootGetters) {
+    const coachId = rootGetters.userId;
+    return state.requests.filter(request => request.coachId === coachId);
+  },
+  hasRequests(_, getters) {
+    return getters.requests && getters.requests.length > 0;
   }
 };
